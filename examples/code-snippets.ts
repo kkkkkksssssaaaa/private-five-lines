288157d05f321f2173a3c00e30126db84ca22803
kkkkkksssssaaaa <kofc312@naver.com>
2023-02-14 23:50:20 +0900

    feat: add code snippets

---
+++ examples/code-snippets.ts
@@ -0,0 +1,25 @@
+// 2. 리팩터링 깊게 들여다 보기
+// 2.2 속도, 유연성 및 안정성 확보
+
+// 예제 2.4 상속을 사용
+interface Bird {
+  hasBeak(): boolean;
+  canFly(): boolean;
+}
+
+class CommonBird implements Bird {
+  hasBeak() {
+    return true;
+  }
+
+  canFly() {
+    return true;
+  }
+}
+
+// 상속
+class Penguin extends CommonBird {
+  canFly() {
+    return false;
+  }
+}
