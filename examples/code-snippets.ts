bb90ab3504f2d560ccaa956c57069db5567ff47e
kkkkkksssssaaaa <kofc312@naver.com>
2023-02-19 01:28:14 +0900

    feat(code-snippets): add example 3.6

--- examples/code-snippets.ts
+++ examples/code-snippets.ts
@@ -63,3 +63,18 @@ function minumum(arr: number[][]) {
 
   return result;
 }
+
+// 예제 3.6 배열의 최소 항목을 찾는 함수
+function minumum_3_6(arr: number[][]) {
+  let result = Number.POSITIVE_INFINITY;
+
+  for (let x = 0; x < arr.length; x++) {
+    for (let y = 0; y < arr[x].length; y++) {
+      if (result > arr[x][y]) 
+        result = arr[x][y];
+    }
+  }
+
+  return result;
+}
+
