025c3eb95fa0943eca85efc82376901b08e8b625
kkkkkksssssaaaa <kofc312@naver.com>
2023-02-19 01:15:37 +0900

    feat(code-snippets): add example 3.1

--- examples/code-snippets.ts
+++ examples/code-snippets.ts
@@ -37,3 +37,16 @@ class CompositionPenguin implements Bird {
     return false;
   }
 }
+
+// 예제 3.1 2차원 배열에 짝수가 존재하는지 확인하는 함수
+function containsEven(arr: number[][]) {
+  for (let x = 0; x < arr.length; x++) {
+    for (let y = 0; y < arr[x].length; y++) {
+      if (arr[x][y] % 2 === 0) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
