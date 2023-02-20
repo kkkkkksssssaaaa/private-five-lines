f21605d662bf8e721c80b9a6901a4db034354a67
kkkkkksssssaaaa <kofc312@naver.com>
2023-02-20 23:27:13 +0900

    feat(input): add input class

--- input.ts
+++ input.ts
@@ -1,4 +1,4 @@
-enum Input {
+enum RawInput {
   RIGHT, LEFT, UP, DOWN
 }
 
@@ -7,4 +7,76 @@ interface Input2 {
   isLeft(): boolean;
   isUp(): boolean;
   isDown(): boolean;
+}
+
+class Right implements Input2 {
+  isRight() {
+    return true;
+  }
+
+  isLeft() {
+    return false;
+  }
+
+  isUp() {
+    return false;
+  }
+
+  isDown() {
+    return false;
+  }
+}
+
+class Left implements Input2 {
+  isRight() {
+    return false;
+  }
+
+  isLeft() {
+      return true;
+  }
+
+  isUp() {
+    return false;
+  }
+
+  isDown() {
+    return false;
+  }
+}
+
+class Up implements Input2 {
+  isRight() {
+    return false;
+  }
+
+  isLeft() {
+    return false;
+  }
+
+  isUp() {
+    return true;
+  }
+
+  isDown() {
+    return false;
+  }
+}
+
+class Down implements Input2 {
+  isRight() {
+    return false;
+  }
+
+  isLeft() {
+    return false;
+  }
+
+  isUp() {
+    return false;
+  }
+
+  isDown() {
+    return true;
+  }
 }
