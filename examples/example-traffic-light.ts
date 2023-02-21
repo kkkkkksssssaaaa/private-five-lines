07a791e1a8545f6953ad4d2c3ebefe5a974ab1a9
kkkkkksssssaaaa <kofc312@naver.com>
2023-02-21 23:20:48 +0900

    feat: add example code for traffic-lights as chapter 4

---
+++ examples/example-traffic-light.ts
@@ -0,0 +1,17 @@
+enum TrafficLight {
+  RED, YELLOW, GREEN
+}
+
+const CYCLE = [
+  TrafficLight.RED,
+  TrafficLight.GREEN,
+  TrafficLight.YELLOW
+];
+
+function updateCarForLight(current: TrafficLight) {
+  if (current === TrafficLight.RED) {
+    // car.stop();
+  } else {
+    // car.drive();
+  }
+}
