44e95fc0b6bb29485620ba603e7dc780adceea01
kkkkkksssssaaaa <kofc312@naver.com>
2023-02-24 00:00:56 +0900

    chore(example-deposit): add database

--- examples/example-deposit.ts
+++ examples/example-deposit.ts
@@ -1,9 +1,20 @@
+const database: any = {
+  find(param: string): number {
+    return 1;
+  },
+
+  updateOne(id: number, amount: number): void {
+    console.log(`${id} updated.`);
+  }
+}
+
 function deposit(to: string, amount: number) {
-  let accountId = databse.find(to);
-  database.updateOne(account, {$inc: {balance: amount}});
+  let accountId = database.find(to);
+  database.updateOne(accountId, {$inc: {balance: amount}});
 }
 
 function transfer(from: string, to: string, amount: number) {
   deposit(from, -amount);
   deposit(to, amount);
 }
+
