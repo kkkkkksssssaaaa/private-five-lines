d6b20665374bb44f2dcfb2dbabf8ca82e0421bf2
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-05 00:39:27 +0900

    refactor(index.ts): calling rest method in updateTile function

--- index.ts
+++ index.ts
@@ -135,10 +135,9 @@ function updateTile(x: number, y: number) {
     && map[y + 1][x].isAir()) {
     map[y + 1][x] = new Box(new Falling());
     map[y][x] = new Air();
-  } else if (map[y][x].isFallingStone()) {
-    map[y][x] = new Stone(new Resting());
-  } else if (map[y][x].isFallingBox()) {
-    map[y][x] = new Box(new Resting());
+  } else if (map[y][x].isFallingStone()
+    || map[y][x].isFallingBox()) {
+    map[y][x].rest();
   }
 }
 
