bbaa7a98fb2d05129907d69b78af09e3f8bb7add
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-05 23:49:47 +0900

    feat(index.ts): calling drop method in updateTime function

--- index.ts
+++ index.ts
@@ -129,11 +129,15 @@ function updateMap() {
 function updateTile(x: number, y: number) {
   if (map[y][x].isStony()
     && map[y + 1][x].isAir()) {
-    map[y + 1][x] = new Stone(new Falling());
+    // 돌이나 상자를 떨어뜨리고 타일을 교체한 후 새로 공기를 주입
+    map[y][x].drop();
+    map[y + 1][x] = map[y][x];
     map[y][x] = new Air();
   } else if (map[y][x].isBoxy()
     && map[y + 1][x].isAir()) {
-    map[y + 1][x] = new Box(new Falling());
+    // 돌이나 상자를 떨어뜨리고 타일을 교체한 후 새로 공기를 주입
+    map[y][x].drop();
+    map[y + 1][x] = map[y][x];
     map[y][x] = new Air();
   } else if (map[y][x].isFalling()) {
     map[y][x].rest();
