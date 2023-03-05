edbdfa3f435ee32881f3f9e25988f65a8712139f
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-06 00:15:03 +0900

    refactor(index.ts): calling update method in updateTime method

--- index.ts
+++ index.ts
@@ -127,15 +127,7 @@ function updateMap() {
 }
 
 function updateTile(x: number, y: number) {
-  if ((map[y][x].canFall())
-    && map[y + 1][x].isAir()) {
-    // 돌이나 상자를 떨어뜨리고 타일을 교체한 후 새로 공기를 주입
-    map[y][x].drop();
-    map[y + 1][x] = map[y][x];
-    map[y][x] = new Air();
-  } else if (map[y][x].isFalling()) {
-    map[y][x].rest();
-  }
+  map[y][x].update(x, y);
 }
 
 function draw() {
