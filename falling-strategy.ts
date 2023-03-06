d75fdc8d0b8cf0464c98d9f453e347af1382166c
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-06 23:42:12 +0900

    refactor($falling-strategy): move update method

--- falling-strategy.ts
+++ falling-strategy.ts
@@ -1,3 +1,20 @@
 class FallingStrategy {
-  
+  constructor(private falling: FallingState) {
+    this.falling = falling;
+  }
+
+  getFalling(): FallingState {
+    return this.falling;
+  }
+
+  update(tile: Tile, x: number, y: number): void {
+    if (map[y + 1][x].isAir()) {
+      // 돌이나 상자를 떨어뜨리고 타일을 교체한 후 새로 공기를 주입
+      this.falling = new Falling();
+      map[y + 1][x] = tile;
+      map[y][x] = new Air();
+    } else if (this.falling.isFalling()) {
+      this.falling = new Resting();
+    }
+  }
 }
--- tile-stone.ts
+++ tile-stone.ts
@@ -1,9 +1,8 @@
 class Stone implements Tile {
-  constructor(
-    private falling: FallingState,
-    private fallingStrategy: FallingStrategy) {
-    this.falling = falling;
-    this.fallingStrategy = new FallingStrategy();
+  private fallingStrategy: FallingStrategy
+
+  constructor(falling: FallingState) {
+    this.fallingStrategy = new FallingStrategy(falling);
   }
 
   isFlux(): boolean {
@@ -53,11 +52,15 @@ class Stone implements Tile {
   }
 
   moveHorizontal(dx: number): void {
-    this.falling.moveHorizontal(this, dx);
+    this.fallingStrategy
+      .getFalling()
+      .moveHorizontal(this, dx);
   }
 
   isFalling(): boolean {
-    return this.falling.isFalling();
+    return this.fallingStrategy
+      .getFalling()
+      .isFalling();
   }
 
   canFall(): boolean {
@@ -65,13 +68,6 @@ class Stone implements Tile {
   }
 
   update(x: number, y: number): void {
-    if (map[y + 1][x].isAir()) {
-      // 돌이나 상자를 떨어뜨리고 타일을 교체한 후 새로 공기를 주입
-      this.falling = new Falling();
-      map[y + 1][x] = this;
-      map[y][x] = new Air();
-    } else if (this.falling.isFalling()) {
-      this.falling = new Resting();
-    }
+    this.fallingStrategy.update(this, x, y);
   }
 }
