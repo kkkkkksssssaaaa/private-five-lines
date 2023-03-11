aeeef8a5dc05405eca819dc17b74c663b50efb7a
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-11 19:47:12 +0900

    feat(example-array): extract field, processElement method

--- example-array.ts
+++ example-array.ts
@@ -1,32 +1,8 @@
 class ArrayMinimum {
   private processor: MinimumProcessor;
 
-  constructor(private accumulator: number) {
-    this.processor = new MinimumProcessor();
-  }
-
-  process(arr: number[]) {
-    for (let i = 0; i < arr.length; i++) {
-      if (this.accumulator > arr[i]) {
-        this.processElement(arr[i]);
-      }
-    }
-
-    return this.accumulator;
-  }
-
-  processElement(e: number) {
-    if (this.accumulator > e) {
-      this.accumulator = e;
-    }
-  }
-}
-
-class ArraySum {
-  private processor: SumProcessor;
-
-  constructor(private accumulator: number) {
-    this.processor = new SumProcessor();
+  constructor(accumulator: number) {
+    this.processor = new MinimumProcessor(accumulator);
   }
 
   process(arr: number[]) {
@@ -38,14 +14,48 @@ class ArraySum {
   }
 
   processElement(e: number) {
-    this.accumulator += e;
+    this.processor.processElement(e);
+  }
+}
+
+class ArraySum {
+  private processor: SumProcessor;
+
+  constructor(accumulator: number) {
+    this.processor = new SumProcessor(accumulator);
+  }
+
+  process(arr: number[]) {
+    for (let i = 0; i < arr.length; i++) {
+      this.processElement(arr[i]);
+    }
+
+    return this.accumulator;
+  }
+
+  processElement(e: number) {
+    this.processor.processElement(e);
   }
 }
 
 class MinimumProcessor {
+  constructor(private accumulator: number) {
 
+  }
+
+  processElement(e: number) {
+    if (this.accumulator > e) {
+      this.accumulator = e;
+    }
+  }
 }
 
 class SumProcessor {
+  constructor(private accumulator: number) {
+    
+  }
 
+  processElement(e: number) {
+    this.accumulator += e;
+  }
 }
