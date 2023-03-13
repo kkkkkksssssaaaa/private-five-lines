60343f89fdf736c08ec3c9a3245481ed615034da
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-13 23:39:32 +0900

    refactor(remove-strategy): change class to interface

--- index.ts
+++ index.ts
@@ -63,7 +63,7 @@ function transformMap() {
 }
 
 function removeLock1() {
-  let shouldRemove = new RemoveStrategy();
+  let shouldRemove = new RemoveLock1();
 
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
--- remove-strategy.ts
+++ remove-strategy.ts
@@ -1,5 +1,15 @@
-class RemoveStrategy {
+interface RemoveStrategy {
+  check(tile: Tile): void;
+}
+
+class RemoveLock1 implements RemoveStrategy {
   check(tile: Tile) {
     return tile.isLock1();
+  }
+}
+
+class RemoveLock2 implements RemoveStrategy {
+  check(tile: Tile) {
+    return tile.isLock2();
   }
 }
