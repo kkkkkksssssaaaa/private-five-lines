304c2b7a702b4ca209e44ec79083c7dd7b8b730f
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-27 01:03:23 +0900

    feat(example-counter): add counter class

--- example-counter.ts
+++ example-counter.ts
@@ -1,12 +1,25 @@
-let counter = 0;
+class Counter {
+  private counter: number = 0;
 
-function incrementCounter() {
-  counter++;
+  getCounter(): number {
+    return this.counter;
+  }
+
+  setCounter(c: number): void {
+    this.counter = c;
+  }
+}
+
+let counter: Counter = new Counter();
+
+function incrementCounter(counter: Counter) {
+  counter.setCounter(
+    counter.getCounter() + 1);
 }
 
 function main() {
   for (let i = 0; i < 20; i++) {
-    incrementCounter();
-    console.log(counter);
+    incrementCounter(counter);
+    console.log(counter.getCounter());
   }
 }
