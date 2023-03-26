23140d092c607c0957b0eaf287c7480b092b7698
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-27 01:05:08 +0900

    refactor(example-counter): move increment method in class

--- example-counter.ts
+++ example-counter.ts
@@ -1,25 +1,16 @@
 class Counter {
   private counter: number = 0;
 
-  getCounter(): number {
-    return this.counter;
-  }
-
-  setCounter(c: number): void {
-    this.counter = c;
+  increment(): void {
+    this.counter++;
+    console.log(this.counter);
   }
 }
 
 let counter: Counter = new Counter();
 
-function incrementCounter(counter: Counter) {
-  counter.setCounter(
-    counter.getCounter() + 1);
-}
-
 function main() {
   for (let i = 0; i < 20; i++) {
-    incrementCounter(counter);
-    console.log(counter.getCounter());
+    counter.increment();
   }
 }
