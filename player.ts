e4e6ba126a79d8cafb448adf825d7d059ae71a1c
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-27 00:06:32 +0900

    feat(player): add private fields

--- player.ts
+++ player.ts
@@ -1,3 +1,20 @@
 class Player {
-  
+  private x: number = 1;
+  private y: number = 1;
+
+  getX(): number {
+    return this.x;
+  }
+
+  getY(): number {
+    return this.y;
+  }
+
+  setX(x: number): void {
+    this.x = x;
+  }
+
+  setY(y: number): void {
+    this.y = y;
+  }
 }
