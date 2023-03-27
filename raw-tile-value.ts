98f1623075e202f7818998ca74803dd81ba82789
kkkkkksssssaaaa <kofc312@naver.com>
2023-03-28 00:37:52 +0900

    feat: add raw tile value interface

---
+++ raw-tile-value.ts
@@ -0,0 +1,26 @@
+interface RawTileValue {
+}
+
+class AirValue implements RawTileValue { }
+
+class FluxValue implements RawTileValue { }
+
+class UnbreakableValue implements RawTileValue { }
+
+class PlayerValue implements RawTileValue { }
+
+class StoneValue implements RawTileValue { }
+
+class FallingStoneValue implements RawTileValue { }
+
+class BoxValue implements RawTileValue { }
+
+class FallingBoxValue implements RawTileValue { }
+
+class Key1Value implements RawTileValue { }
+
+class Lock1Value implements RawTileValue { }
+
+class Key2Value implements RawTileValue { }
+
+class Lock2Value implements RawTileValue { }
